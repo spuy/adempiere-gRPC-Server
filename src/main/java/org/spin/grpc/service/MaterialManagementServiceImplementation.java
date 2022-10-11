62ce66364b158d344731e86afe4b381c2d579131
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-10-11 17:04:06 -0400

    fix: Product stock on lines or header document.

--- src/main/java/org/spin/grpc/service/MaterialManagementServiceImplementation.java
+++ src/main/java/org/spin/grpc/service/MaterialManagementServiceImplementation.java
@@ -19,9 +19,19 @@ import java.util.List;
 import java.util.Properties;
 
 import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.I_C_Invoice;
+import org.compiere.model.I_C_InvoiceLine;
+import org.compiere.model.I_C_Order;
+import org.compiere.model.I_C_OrderLine;
+import org.compiere.model.I_M_Requisition;
+import org.compiere.model.I_M_RequisitionLine;
+import org.compiere.model.MInvoiceLine;
+import org.compiere.model.MOrderLine;
+import org.compiere.model.MRequisitionLine;
 import org.compiere.model.MRole;
 import org.compiere.model.MTable;
 import org.compiere.util.CLogger;
+import org.compiere.util.Env;
 import org.compiere.util.Util;
 import org.spin.base.util.ContextManager;
 import org.spin.base.util.DictionaryUtil;
@@ -72,20 +82,17 @@ public class MaterialManagementServiceImplementation extends MaterialManagementI
 		StringBuffer whereClause = new StringBuffer(" WHERE 1=1 ");
 
 		//	For dynamic condition
-		List<Object> params = new ArrayList<>(); // includes on filters criteria
+		List<Object> parametersList = new ArrayList<>(); // includes on filters criteria
 		if (!Util.isEmpty(request.getTableName(), true)) {
-		    int recordId = request.getRecordId();
-		    if (recordId <= 0) {
-		        if (Util.isEmpty(request.getRecordUuid())) {
-		            
-		        }
-		        recordId = RecordUtil.getIdFromUuid(request.getTableName(), request.getRecordUuid(), null);
-		    }
-			//	Add includes first AND
-			whereClause.append(" AND ")
-				.append(" EXISTS(SELECT 1 FROM " + request.getTableName())
-				.append(" WHERE RV_Storage.M_Product_ID = ")
-				.append(request.getTableName() + ".M_Product_ID)");
+			int recordId = request.getRecordId();
+			if (recordId <= 0) {
+				recordId = RecordUtil.getIdFromUuid(request.getTableName(), request.getRecordUuid(), null);
+			}
+			if (recordId <= 0) {
+				throw new AdempiereException("@Record_ID@ @NotFound@");
+			}
+			String where = getWhereClause(request.getTableName(), recordId, parametersList);
+			whereClause.append(where);
 		}
 		sql.append(whereClause); 
 
@@ -105,10 +112,10 @@ public class MaterialManagementServiceImplementation extends MaterialManagementI
 		ListEntitiesResponse.Builder builder = ListEntitiesResponse.newBuilder();
 
 		//	Count records
-		count = RecordUtil.countRecords(parsedSQL, tableName, params);
+		count = RecordUtil.countRecords(parsedSQL, tableName, parametersList);
 		//	Add Row Number
 		parsedSQL = RecordUtil.getQueryWithLimit(parsedSQL, limit, offset);
-		builder = RecordUtil.convertListEntitiesResult(MTable.get(context, tableName), parsedSQL, params);
+		builder = RecordUtil.convertListEntitiesResult(MTable.get(context, tableName), parsedSQL, parametersList);
 		//	
 		builder.setRecordCount(count);
 		//	Set page token
@@ -120,6 +127,92 @@ public class MaterialManagementServiceImplementation extends MaterialManagementI
 		builder.setNextPageToken(ValueUtil.validateNull(nexPageToken));
 
 		return builder;
+	}
+
+	public static String getWhereClause(String tableName, int recordId, List<Object> parametersList) {
+		String where = "";
+
+		switch (tableName) {
+			case I_M_Requisition.Table_Name:
+			case I_M_RequisitionLine.Table_Name:
+				where = getWhereClasuseByRequisition(tableName, recordId, parametersList);
+				break;
+			case I_C_Order.Table_Name:
+			case I_C_OrderLine.Table_Name:
+				where = getWhereClasuseByOrder(tableName, recordId, parametersList);
+				break;
+			case I_C_Invoice.Table_Name:
+			case I_C_InvoiceLine.Table_Name:
+			    where = getWhereClasuseByInvoice(tableName, recordId, parametersList);
+				break;
+			default:
+			    where = " AND "
+					+ " EXISTS(SELECT 1 FROM " + tableName
+					+ " WHERE RV_Storage.M_Product_ID = "
+					+ tableName + ".M_Product_ID"
+					+ " AND "
+					+ tableName + "." + tableName + "_ID = ?)"
+				;
+				parametersList.add(recordId);
+				break;
+		}
+
+		return where;
+	}
+
+
+	public static String getWhereClasuseByRequisition(String tableName, int recordId, List<Object> parametersList) {
+		int requisitionId = recordId;
+		StringBuffer whereClause = new StringBuffer();
+		if (tableName.equals(I_M_Requisition.Table_Name)) {
+		} else if (tableName.equals(I_M_RequisitionLine.Table_Name)) {
+			MRequisitionLine requisitionLine = new MRequisitionLine(Env.getCtx(), recordId, null);
+			requisitionId = requisitionLine.getM_Requisition_ID();
+		}
+		whereClause.append(" AND ")
+			.append("EXISTS(SELECT 1 FROM M_RequisitionLine WHERE ")
+			.append("RV_Storage.M_Product_ID = M_RequisitionLine.M_Product_ID ")
+			.append("AND M_RequisitionLine.M_Requisition_ID = ?) ")
+		;
+		parametersList.add(requisitionId);
+
+		return whereClause.toString();
+	}
+
+	public static String getWhereClasuseByOrder(String tableName, int recordId, List<Object> parametersList) {
+		int orderId = recordId;
+		StringBuffer whereClause = new StringBuffer();
+		if (tableName.equals(I_C_Order.Table_Name)) {
+		} else if (tableName.equals(I_C_OrderLine.Table_Name)) {
+			MOrderLine orderLine = new MOrderLine(Env.getCtx(), recordId, null);
+			orderId = orderLine.getC_Order_ID();
+		}
+		whereClause.append(" AND ")
+			.append("EXISTS(SELECT 1 FROM C_OrderLine WHERE ")
+			.append("RV_Storage.M_Product_ID = C_OrderLine.M_Product_ID ")
+			.append("AND C_OrderLine.C_Order_ID = ?) ")
+		;
+		parametersList.add(orderId);
+
+		return whereClause.toString();
+	}
+
+	public static String getWhereClasuseByInvoice(String tableName, int recordId, List<Object> parametersList) {
+		int invoiceId = recordId;
+		StringBuffer whereClause = new StringBuffer();
+		if (tableName.equals(I_C_Invoice.Table_Name)) {
+		} else if (tableName.equals(I_C_InvoiceLine.Table_Name)) {
+			MInvoiceLine invoiceLine = new MInvoiceLine(Env.getCtx(), recordId, null);
+			invoiceId = invoiceLine.getC_Invoice_ID();
+		}
+		whereClause.append(" AND ")
+			.append("EXISTS(SELECT 1 FROM C_InvoiceLine WHERE ")
+			.append("RV_Storage.M_Product_ID = C_InvoiceLine.M_Product_ID ")
+			.append("AND C_InvoiceLine.C_Invoice_ID = ?) ")
+		;
+		parametersList.add(invoiceId);
+
+		return whereClause.toString();
 	}
 
 }
