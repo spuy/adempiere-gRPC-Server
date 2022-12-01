9357cf3fb79c86c7065fb8a0fb88001f6c09f280
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-12-01 16:33:42 -0400

    fix: Search with sql IN validation. (#157)
    * fix: Search with sql IN validation.
    
    * improve empty dynamic validation.

--- src/main/java/org/spin/base/util/DictionaryUtil.java
+++ src/main/java/org/spin/base/util/DictionaryUtil.java
@@ -1,17 +1,17 @@
 /*************************************************************************************
  * Product: Adempiere ERP & CRM Smart Business Solution                              *
- * This program is free software; you can redistribute it and/or modify it    		 *
+ * This program is free software; you can redistribute it and/or modify it           *
  * under the terms version 2 or later of the GNU General Public License as published *
- * by the Free Software Foundation. This program is distributed in the hope   		 *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 		 *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           		 *
- * See the GNU General Public License for more details.                       		 *
- * You should have received a copy of the GNU General Public License along    		 *
- * with this program; if not, write to the Free Software Foundation, Inc.,    		 *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     		 *
- * For the text or an alternative of this public license, you may reach us    		 *
+ * by the Free Software Foundation. This program is distributed in the hope          *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied        *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ * See the GNU General Public License for more details.                              *
+ * You should have received a copy of the GNU General Public License along           *
+ * with this program; if not, write to the Free Software Foundation, Inc.,           *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                            *
+ * For the text or an alternative of this public license, you may reach us           *
  * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, S.A. All Rights Reserved. *
- * Contributor(s): Yamel Senih www.erpya.com				  		                 *
+ * Contributor(s): Yamel Senih www.erpya.com                                         *
  *************************************************************************************/
 package org.spin.base.util;
 
@@ -213,14 +213,26 @@ public class DictionaryUtil {
 	 * @return {String}
 	 */
 	public static String getValidationCodeWithAlias(String tableAlias, String dynamicValidation) {
-		String validationCode = dynamicValidation;
+		if (Util.isEmpty(dynamicValidation, true)) {
+			return "";
+		}
 
-		Matcher matcherTableAliases = Pattern.compile("" + tableAlias + ".", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)
+		Matcher matcherTableAliases = Pattern.compile(
+				tableAlias + "\\.",
+				Pattern.CASE_INSENSITIVE | Pattern.DOTALL
+			)
 			.matcher(dynamicValidation);
+
+		String validationCode = dynamicValidation;
 		if (!matcherTableAliases.find()) {
-			Pattern patternColumnName = Pattern.compile("(\\w+)(\\s+){0,1}=", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+			String sqlOperators = "(<>|<=|>=|!=|<|=|>|NOT\\s+IN|IN|NOT\\s+BETWEEN|BETWEEN|NOT\\s+LIKE|LIKE|IS\\s+NULL|IS\\s+NOT\\s+NULL)";
+			// columnName = value
+			Pattern patternColumnName = Pattern.compile(
+				"(\\w+)(\\s+){0,1}" + sqlOperators,
+				Pattern.CASE_INSENSITIVE | Pattern.DOTALL
+			);
 			Matcher matchColumnName = patternColumnName.matcher(validationCode);
-			validationCode = matchColumnName.replaceAll(tableAlias + ".$1="); // $&
+			validationCode = matchColumnName.replaceAll(tableAlias + ".$1$2$3"); // $&
 		}
 
 		return validationCode;
