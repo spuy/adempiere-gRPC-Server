292daa099eaf756511aa4753cf4331b195e61afe
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2024-07-24 15:48:54 -0400

    Feat: Support Send Notification (#827)
    * Feat: Support Send Notification
    
    * Change proto
    
    * Add Recipient to Notification
    
    * change proto
    
    * minimal changes

--- src/main/java/org/spin/grpc/service/SendNotifications.java
+++ src/main/java/org/spin/grpc/service/SendNotifications.java
@@ -11,18 +11,25 @@ import org.compiere.model.MUser;
 import org.compiere.model.Query;
 import org.compiere.util.CLogger;
 import org.compiere.util.Env;
+import org.compiere.util.Util;
 import org.spin.backend.grpc.common.ListLookupItemsResponse;
 import org.spin.backend.grpc.common.LookupItem;
 import org.spin.backend.grpc.send_notifications.ListNotificationsTypesRequest;
 import org.spin.backend.grpc.send_notifications.ListNotificationsTypesResponse;
 import org.spin.backend.grpc.send_notifications.ListUsersRequest;
+import org.spin.backend.grpc.send_notifications.SendNotificationResponse;
 import org.spin.backend.grpc.send_notifications.NotifcationType;
+import org.spin.backend.grpc.send_notifications.SendNotificationRequest;
 import org.spin.backend.grpc.send_notifications.SendNotificationsGrpc.SendNotificationsImplBase;
+import org.spin.base.util.AccessUtil;
 import org.spin.base.util.LookupUtil;
+import org.spin.queue.notification.DefaultNotifier;
+import org.spin.queue.util.QueueLoader;
 import org.spin.service.grpc.util.value.ValueManager;
 
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import io.vavr.collection.Array;
 
 public class SendNotifications extends  SendNotificationsImplBase{
     /**	Logger			*/
@@ -51,7 +58,7 @@ public class SendNotifications extends  SendNotificationsImplBase{
 	
 	private ListLookupItemsResponse.Builder ListUsers(ListUsersRequest request) {
 		//	Add DocStatus for validation
-		final String validationCode = "Email IS NOT NULL ";
+		final String validationCode = "NotificationType <> 'X' ";
 		Query query = new Query(
 			Env.getCtx(),
 			I_AD_User.Table_Name,
@@ -165,5 +172,89 @@ public class SendNotifications extends  SendNotificationsImplBase{
 		});
 
 		return builderList;
+	}
+
+	public void sendNotification(SendNotificationRequest request, StreamObserver<SendNotificationResponse> responseObserver) {
+		try {
+			if (request == null) {
+				throw new AdempiereException("Object Request Null");
+			}
+			SendNotificationResponse.Builder builder = sendNotification(request);
+			responseObserver.onNext(builder.build());
+			responseObserver.onCompleted();
+		} catch (Exception e) {
+			log.severe(e.getLocalizedMessage());
+			e.printStackTrace();
+			responseObserver.onError(
+				Status.INTERNAL
+					.withDescription(e.getLocalizedMessage())
+					.withCause(e)
+					.asRuntimeException()
+			);
+		}
+	}
+
+	private SendNotificationResponse.Builder sendNotification(SendNotificationRequest request) {
+		MUser currentUser = MUser.get(Env.getCtx());
+
+		int userId = request.getUserId();
+
+		//	Validate user
+		if(userId <= 0 && request.getUserId() <= 0) {
+			userId = currentUser.getAD_User_ID();
+		}
+
+		if(Util.isEmpty(request.getNotificationType(), true)) {
+			throw new AdempiereException("@NotifcationType@ @NotFound@");
+		}
+		
+		// Validate the Body
+		if(Util.isEmpty(request.getBody(), true)) {
+			throw new AdempiereException("@Body@ @Mandatory@");
+		}
+
+		// Validate the Title
+		if(Util.isEmpty(request.getTitle(), true)) {
+			throw new AdempiereException("@Title@ @Mandatory@");
+		}
+
+		// Validate the list of recipients
+		if (request.getRecipientsList() == null || request.getRecipientsList().isEmpty()) {
+			throw new AdempiereException("@Recipinets@ @Mandatory@");
+		}
+		StringBuffer error = new StringBuffer();
+		request.getRecipientsList().forEach(recipient -> {
+			if (recipient.getContactId() <= 0 && Util.isEmpty(recipient.getAccountName(), true)) {
+				error.append("Recipient does not have a valid Contact ID (" + recipient.getContactId() + ") Account Name (" + recipient.getAccountName() + ") ");
+			}
+		});
+
+		if (error.length() > 0) {
+			throw new AdempiereException("Errors in the recipient list:\n" + error.toString());
+		}
+
+
+		//	Get instance for notifier
+		DefaultNotifier notifier = (DefaultNotifier) QueueLoader.getInstance().getQueueManager(DefaultNotifier.QUEUETYPE_DefaultNotifier)
+				.withContext(Env.getCtx());
+
+		//	Send notification to queue
+		notifier
+			.clearMessage()
+			.withApplicationType(request.getNotificationType())
+			.withUserId(userId)
+			.withText(request.getBody())
+			.withDescription(request.getTitle());
+
+			// Add Recipient to Notification
+			request.getRecipientsList().forEach(recipients -> {
+				notifier.addRecipient(recipients.getContactId(),recipients.getAccountName());
+			});
+			//	Attachment
+			// notifier.addAttachment(request.getAttachments());
+
+			//	Add to queue
+			notifier.addToQueue();
+		return SendNotificationResponse.newBuilder();
 	}
 }
